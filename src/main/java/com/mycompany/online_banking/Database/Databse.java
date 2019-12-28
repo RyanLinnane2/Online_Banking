67e98eeeabe904f7c3dc4ca253f8ac5a6cbaa0f3
tomkurzynski <tomasz.kurzynski@gmail.com>
2019-12-28 18:16:28 +0000

    AccountServices/createAccount modified, transaction/service/resource updated

--- src/main/java/com/mycompany/online_banking/Database/Databse.java
+++ src/main/java/com/mycompany/online_banking/Database/Databse.java
@@ -12,80 +12,65 @@ import com.mycompany.online_banking.Model.User;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 /**
  *
- * @author The Red Setters
+ * @author x18145761, x18137695
  */
 public class Databse {
-    
+
     //Create Lists
     private static List<User> userList = new ArrayList<>();
     private static List<Account> accountList = new ArrayList<>();
     private static List<Transaction> transList = new ArrayList<>();
     private static Account account = new Account();
-    private static boolean db = true; 
+    private static boolean db = true;
 
-    
     //constructor
     public Databse() {
-        
-        if(db){
-        
-       //--------------USER TRANSACTYIONS------------------------//
-       List<Transaction> trans1 = new ArrayList<>();
-       List<Transaction> trans2 = new ArrayList<>();
-               //String date, String description, double newBalance, String debit_credit)
-        Transaction transHistory1 = new Transaction("20-12-19","food",500,"debit");
-        Transaction transHistory2 = new Transaction("21-12-19","food",200,"debit");
-                
 
-        //--------------USER ACCOUNTS--------------------//
-       
-       //store user 1's accounts
-        List<Account> userOneAccount = new ArrayList<>();
-        //store user 2's accounts
-        List<Account> userTwoAccount = new ArrayList<>();
+        if (db) {
 
-        //initialise accounts for user 1 and 2
-        //accountID is used to track location of users account in the arraylist1
-       Account userOneAccount1 = new Account(1,123,5000,000123, trans1);  
-       Account userTwoAccount1 = new Account(2,456,5000,001024, trans1);
-       Account userOneaccount2 = new Account(3,321,1000,000124,trans2);
-       
-       // add initialised accounts to user account arraylist
-       
-       userOneAccount.add(userOneAccount1);
-       userOneAccount.add(userOneaccount2);
-       userTwoAccount.add(userTwoAccount1); 
-       
-               
-    
-       
-       //add to general overall account db
-       accountList.add(userOneAccount1);
-       accountList.add(userOneaccount2);
-       accountList.add(userTwoAccount1);
-       
-        //------------------CREATE USERS--------------------//
-        //initialise users name,address,email,password,userID,List<Account>
-        User user1 = new User("John", "Dublin", "john@gmail.com","password",1,userOneAccount);
-        User user2 = new User("Mark", "Dublin", "mark@gmail.com","password",2,userTwoAccount);
+            //--------------USER TRANSACTYIONS------------------------//
+//       List<Transaction> trans1 = new ArrayList<>();
+//       List<Transaction> trans2 = new ArrayList<>();
+//               //String date, String description, double newBalance, String debit_credit)
+//        Transaction transHistory1 = new Transaction("20-12-19","food",500,"debit");
+//        Transaction transHistory2 = new Transaction("21-12-19","food",200,"debit");
+            //--------------USER ACCOUNTS--------------------//
+            //store user 1's accounts
+            List<Account> userOneAccount = new ArrayList<>();
+            //store user 2's accounts
+            List<Account> userTwoAccount = new ArrayList<>();
 
-        userList.add(user1);
-        accountList = userOneAccount;
-        userList.add(user2);
-        accountList = userTwoAccount;
- 
-        
-         db = false;
+            //initialise accounts for user 1 and 2
+            //accountID is used to track location of users account in the arraylist1
+            Account userOneAccount1 = new Account(1, 123, 5000, 000123);
+            Account userTwoAccount1 = new Account(2, 456, 5000, 001024);
+            Account userOneaccount2 = new Account(3, 321, 1000, 000124);
+
+            // add initialised accounts to user account arraylist
+            userOneAccount.add(userOneAccount1);
+            userOneAccount.add(userOneaccount2);
+            userTwoAccount.add(userTwoAccount1);
+
+            //add to general overall account db
+            accountList.add(userOneAccount1);
+            accountList.add(userOneaccount2);
+            accountList.add(userTwoAccount1);
+
+            //------------------CREATE USERS--------------------//
+            //initialise users name,address,email,password,userID,List<Account>
+            User user1 = new User("John", "Dublin", "john@gmail.com", "password", 1, userOneAccount);
+            User user2 = new User("Mark", "Dublin", "mark@gmail.com", "password", 2, userTwoAccount);
+
+            userList.add(user1);
+//        accountList = userOneAccount;
+            userList.add(user2);
+//        accountList = userTwoAccount;
+
+            db = false;
         }
-            
-      
-            
-        
-        
+
     }
 
     public static List<User> getUserList() {
@@ -99,9 +84,5 @@ public class Databse {
     public static List<Transaction> getTransList() {
         return transList;
     }
-
-  
-
-    
 
 }
--- src/main/java/com/mycompany/online_banking/Resources/UserResource.java
+++ src/main/java/com/mycompany/online_banking/Resources/UserResource.java
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 package com.mycompany.online_banking.Resources;
+
 import com.mycompany.online_banking.Database.Databse;
 import com.mycompany.online_banking.Model.User;
 import com.mycompany.online_banking.Model.Account;
 import com.mycompany.online_banking.Model.Transaction;
 import com.mycompany.online_banking.Services.AccountServices;
 import com.mycompany.online_banking.Services.UserService;
-
-
 
 import java.util.List;
 import javax.ws.rs.Consumes;
@@ -25,30 +24,28 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import com.google.gson.*;
 
-
 /**
  *
- * @author Ryan
+ * @author x18145761, x18137695
  */
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
     UserService userService = new UserService();
     Databse userDB = new Databse();
-    
 
-     // curl -v -X POST http://localhost:49000/api/users/fetch/1 
-  
+    // curl -v -X POST http://localhost:49000/api/users/fetch/1 
     @GET
     @Path("/fetch/{userId}")
     @Produces(MediaType.APPLICATION_XML)
-    public User userById(@PathParam("userId")int userId){
+    public User userById(@PathParam("userId") int userId) {
         System.out.println("Fetching User information...");
         return userService.getUserById(userId);
     }
-    
-       // curl -v -X POST http://localhost:49000/api/users/createUser
+
+    // curl -v -X POST http://localhost:49000/api/users/createUser
     /*
     @POST
     @Path("/createUser")
@@ -57,7 +54,7 @@ public class UserResource {
         System.out.println("Creating new User...");
         return userService.createUser(u);
     }
-    */
+     */
     // curl -v -X POST http://localhost:49000/api/users/ -d {"name": "Bob","address": "123 Dawson St, Dublin 2","email": "email@email.com",password": "pass123"}
     /* In Postman...Body -> raw
     *   {
@@ -66,16 +63,16 @@ public class UserResource {
     *   "email": "email@email.com",
     *   "password": "pass123"
     *   }
-    */
+     */
     @POST
     public Response addUser(String body) {
         Gson gson = new Gson();
         User a = gson.fromJson(body, User.class);
         userService.createUser(a);
-        return Response.status(Response.Status.CREATED).entity(gson.toJson(a)).build();   
-        
+        return Response.status(Response.Status.CREATED).entity(gson.toJson(a)).build();
+
     }
-    
+
     //curl -vi -X GET -G "http://localhost:49000/api/users/all"
     @GET
     @Path("/all")
@@ -84,7 +81,7 @@ public class UserResource {
         System.out.println("Fetching user information...");
         return userService.getAllUsers();
     }
-    
+
     //curl -vi -X GET -G "http://localhost:49000/api/users/allj"
     @GET
     @Path("/allj")
@@ -93,9 +90,7 @@ public class UserResource {
         Gson gson = new Gson();
         System.out.println("Fetching user information...");
         List<User> a = userService.getAllUsers();
-        return Response.status(Response.Status.CREATED).entity(gson.toJson(a)).build(); 
-    }  
-   
+        return Response.status(Response.Status.CREATED).entity(gson.toJson(a)).build();
+    }
 
-    
 }
